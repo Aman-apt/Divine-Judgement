a242ac785b55a19993f9c45758fded8fde421f1f
Aman-apt <amanyadav665078@gmail.com>
2026-05-30 16:17:57 +0530

    Fixed the in-flight request with semaphore

--- load_tester/request.py
+++ load_tester/request.py
@@ -24,25 +24,28 @@ scalability -- How well the system scales as the concurrent users increases .
 # or i can createa a different function to do just that.
 
 import asyncio
+from sys import exception
 import aiohttp
 
+# so the concurrenty problem is somewhat solved but we have anther proble which was expected .
+# That is ClientTimeOutError :- As number of request increases time also increases . 
+# How do i solve that . Q1) is this even problem that can be solved or it's is generally expected . ? 
+# If it is solveable i need something ? i don't have any idea how am i going to solve this problem any how ?
+# Well i have 10,000 task running, how many are actually doing network I/O at any single moment ?
+# Not all 10,000. The event loop can only handle what the OS reports as ready via epoll 
+# how do i control exactly how many requests are in-flight at any moment ?
 
-# Make 20 concurrent request to the same url and receive the 200
-# so function_make_single_request returns an int and we can't create task or gather task on int , we need iterable but how?
-# should we use while loop for sessiong . ? 
-# Let's break down the problem here , we have to do three things here:- 1) A succesfull single req , 2) Make this request concurrent?
-# What do i made my concurrent that non-blocking i meant by that . Let's try to make this happnen but how ? 
-# No clue :-
-
-async def make_single_request(url: str, session: aiohttp.ClientSession) -> int:
+async def make_single_request(url: str, session: aiohttp.ClientSession, semaphore: asyncio.Semaphore) -> int:
     # should we do this . but what would happen here .
-    async with session.get(url) as response:
-        success, failure = 0, 0
     
-        if response.status == 200:
-            return response.status
-        else:
-            pass
+    async with semaphore:
+        async with session.get(url) as response:
+            success, failure = 0, 0
+        
+            if response.status == 200:
+                return response.status
+            else:
+                return response.status
 
 async def worker(user_count: int):
     pass
@@ -59,10 +62,12 @@ async def make_conc_request(url: str, num_req: int = None) -> int:
 
     task_result = []
 
-    timeout = aiohttp.ClientTimeout(total=20)
+    semphaphore = asyncio.Semaphore(40)
+
+    timeout = aiohttp.ClientTimeout(total=60)
     async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
-        concurrent_list = [make_single_request(url, session) for _ in range(num_req)] #this is naive appraoch but it works
-        task = asyncio.gather(*concurrent_list)
+        concurrent_list = [make_single_request(url, session, semphaphore) for _ in range(num_req)] #this is naive appraoch but it works
+        task = asyncio.gather(*concurrent_list, return_exceptions=True)
         result = await task
 
         success, failure = 0, 0
@@ -72,12 +77,12 @@ async def make_conc_request(url: str, num_req: int = None) -> int:
                 failure += 1
             elif status == 200:
                 success += 1
-    return (f"Failuer{failure} :: Success {success}")
+    return (f"Failuer: {failure} -- Success :{success}")
 
 async def main():
-    url_path = "https://techcruch.com"
-    request_per_test = 10
-    single_request = await make_conc_request(url_path, request_per_test)
+    url = "https://techcrunch.com"
+    req_per_test = 2000
+    single_request = await make_conc_request(url, req_per_test)
     print(single_request)
 
 asyncio.run(main())
